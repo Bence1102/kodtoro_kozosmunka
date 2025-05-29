7c022c59d26d74ad85c6a06923b3cc4a1f68dcc4
MGergo20 <medgyaszayg3@gmail.com>
2025-05-29 13:58:21 +0200

    lassan

--- Generalt.js
+++ Generalt.js
@@ -1,23 +1,20 @@
-
-export default class Generalt {
-  #generaltLista = [];
+export default class Generalt{
+  #generaltLista=[];
   #szElem;
-  constructor(generaltLista, szElem) {
-    this.#generaltLista = generaltLista;
-    this.#szElem = document.querySelector(".generalt .jateksor");
-    this.megjelenit();
+  constructor(generaltLista){
+      this.#generaltLista=generaltLista;
+      this.#szElem=document.querySelector(".generalt .jateksor")
+      this.megjelenit();
   }
-  megjelenit() {
-    let html = `<div class="visszajelzes"></div>
-    <div class="sor">
-     <div class="Elem"  style="background-color: ${this.#generaltLista[0]}"></div>
-     <div class="Elem" style="background-color: ${this.#generaltLista[1]}"></div>
-     <div class="Elem" style="background-color: ${this.#generaltLista[2]}"></div>
-     <div class="Elem" style="background-color: ${this.#generaltLista[3]}"></div>
-   </div>
-    `;
-    this.#szElem.insertAdjacentHTML("beforeend", html);
+  megjelenit(){
+      let html = `<div class="visszajelzes"></div>
+                  <div class="sor">
+                      <div class="elem" style="background-color:${this.#generaltLista[0]}"}></div>
+                      <div class="elem" style="background-color:${this.#generaltLista[1]}"}></div>
+                      <div class="elem" style="background-color:${this.#generaltLista[2]}"}></div>
+                      <div class="elem" style="background-color:${this.#generaltLista[3]}"}></div>
+                  </div>`;
+      this.#szElem.insertAdjacentHTML("beforeend", html);
   }
-
+  
 }
-
--- index.js
+++ index.js
@@ -1,11 +1,2 @@
-
-
-import Generalt from "./Generalt.js";
-import Tipp from "./Tipp.js";
-import { generaltLista, tippListaM } from "./listak.js";
-
-const jatekterElem = document.querySelector(".jatekter");
-const tipp = new Tipp(tippListaM, jatekterElem);
-
-const generaltElem = document.querySelector(".generalt");
-const general = new Generalt(generaltElem, generaltLista)
+import Jatek from "./Jatek.js";
+new Jatek()
