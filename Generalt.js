a34948546c88153c7b23a4955eaf5ab5d18656c7
MGergo20 <medgyaszayg3@gmail.com>
2025-05-29 13:24:49 +0200

    13:24

--- Generalt.js
+++ Generalt.js
@@ -1,16 +1,21 @@
-import { szinekLista } from "./listak.js";
-export default class Generalt{
-    #generaltLista=[];
-    #szElem;
-    constructor(generaltLista){
-        this.#generaltLista=generaltLista;
-        this.#szElem=document.querySelector(".generalt .jateksor .sor")
-    }
-    megjelenit(){
-        for (let index = 0; index < this.#generaltLista.length; index++) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [lista[i], lista[j]] = [lista[j], lista[i]]; 
-            
-        }
-    }
+
+export default class Generalt {
+  #generaltLista = [];
+  #szElem;
+  constructor(generaltLista) {
+    this.#generaltLista = generaltLista;
+    this.#szElem = document.querySelector(".generalt .jateksor .sor")
+  }
+  megjelenit() {
+    let html = `
+             <div class="generalt">
+              <div class="Elem"  style="background-color: ${this.generaltLista[0]}"></div>
+              <div class="Elem" style="background-color: ${this.generaltLista[1]}"></div>
+              <div class="Elem" style="background-color: ${this.generaltLista[2]}"></div>
+              <div class="Elem" style="background-color: ${this.generaltLista[3]}"></div>
+            </div>
+      `
+    this.#szElem.insertAdjacentHTML("beforeend", html);
+  };
 }
+  
