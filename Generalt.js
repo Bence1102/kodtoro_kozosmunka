149ab6c7b2b0ca3acb4b8484187b551d3efe693d
MGergo20 <medgyaszayg3@gmail.com>
2025-05-29 11:02:07 +0200

    11:02

---
+++ Generalt.js
@@ -0,0 +1,13 @@
+import { generaltLista } from "./listak.js";
+export default class General {
+  #generaltLista = [];
+  #szElem;
+  constructor(generaltLista) {
+    this.#szElem = szinElem;
+    this.#generaltLista = generaltLista;
+    megjelenit();
+  }
+  megjelenit(){
+    
+  }
+}
