bc927cf50d5b02285ca9a14ccac8801da98f70ce
Bence1102 <szente.bence04@gmail.com>
2025-05-29 13:57:29 +0200

    jatek

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
+import Jatek from "./Jatek.js";
 
-
-import Generalt from "./Generalt.js";
-import Tipp from "./Tipp.js";
-import { generaltLista, tippListaM } from "./listak.js";
-
-const jatekterElem = document.querySelector(".jatekter");
-const tipp = new Tipp(tippListaM, jatekterElem);
-
-const generaltElem = document.querySelector(".generalt");
-const general = new Generalt(generaltElem, generaltLista)
+new Jatek();
