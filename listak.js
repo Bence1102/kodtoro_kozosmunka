5390d176fa9273481c1e6e1a3e4f0c7406b1b7b5
Bence1102 <szente.bence04@gmail.com>
2025-05-29 10:25:45 +0200

    listak

---
+++ listak.js
@@ -0,0 +1,26 @@
+export const szinLista = ["red","green","blue","yellow","purple","orange"];
+export const generaltLista = ["white", "white","white","white"];
+export const visszajelzesLista = [
+    ["grey", "grey","grey","grey"]
+    ["grey", "grey","grey","grey"]
+    ["grey", "grey","grey","grey"]
+    ["grey", "grey","grey","grey"]
+    ["grey", "grey","grey","grey"]
+    ["grey", "grey","grey","grey"]
+    ["grey", "grey","grey","grey"]
+    ["grey", "grey","grey","grey"]
+    ["grey", "grey","grey","grey"]
+    ["grey", "grey","grey","grey"]
+];
+export const tippLista = [
+    ["white", "white","white","white"]
+    ["white", "white","white","white"]
+    ["white", "white","white","white"]
+    ["white", "white","white","white"]
+    ["white", "white","white","white"]
+    ["white", "white","white","white"]
+    ["white", "white","white","white"]
+    ["white", "white","white","white"]
+    ["white", "white","white","white"]
+    ["white", "white","white","white"]
+];
