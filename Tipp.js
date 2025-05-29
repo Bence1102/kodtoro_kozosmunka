c8d04107a6b21ea1266d0906a7a2b4e6dec001f5
Bence1102 <szente.bence04@gmail.com>
2025-05-29 12:15:07 +0200

    tipp folyamatban meg a visszajelzés

--- Tipp.js
+++ Tipp.js
@@ -1,12 +1,25 @@
+import { tippLista } from "./listak.js";
+
 export default class Tipp {
-    #tippLista = [];
-    #szElem;
-    constructor(tippLista) {
-        this.#szElem=szElem;
+
+    #tippLista=[]; 
+    #szElem;   
+
+    constructor(tippLista, szElem) {
         this.#tippLista = tippLista;
-        this.#esemelykezelo()
+        this.#szElem = szElem;     
     }
 
-    
-    
+    megjelenit() {
+        
+        this.#szElem.innerHTML = "";
+
+        
+        this.#tippLista.forEach((tipp) => {
+            const tippElem = document.createElement("div");
+            tippElem.textContent = tipp; 
+            tippElem.classList.add("tipp-item");
+            this.#szElem.appendChild(tippElem); 
+        });
+    }
 }
