7d882ae013986e1f1c37dd8bfedcf5d6db4a194d
Bence1102 <szente.bence04@gmail.com>
2025-05-29 11:03:24 +0200

    tipp

---
+++ Tipp.js
@@ -0,0 +1,12 @@
+export default class Tipp {
+    #tippLista = [];
+    #szElem;
+    constructor(tippLista) {
+        this.#szElem=szElem;
+        this.#tippLista = tippLista;
+        this.#esemelykezelo()
+    }
+
+    
+    
+}
